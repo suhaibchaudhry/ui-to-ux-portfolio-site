3a6e3f011100abc115126dbd6cac28024baad40a
Asad Hasan <asad@dadesigners.com>
2015-08-23 21:04:08 -0500

    Add app.js

---
+++ assets/js/dependencies/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+			$('#fullpage').fullpage({
+
+				verticalCentered: true,
+
+				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE'],
+
+				afterRender: function(){
+
+
+
+
+
+					//playing the video
+
+					$('video').get(0).play();
+
+				}
+
+			});
+
+		});
